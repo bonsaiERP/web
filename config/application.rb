03207bf009afe5469f9596c4664b24912687ef50
Boris Barroso <boriscyber@gmail.com>
2013-09-13 14:02:10 -0400

    Added font awesome 3.2

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module BonsaiErpSite
     config.i18n.default_locale = :es
 
     config.assets.paths << Rails.root.join('vendor', 'assets')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'font')
 
     config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
   end
