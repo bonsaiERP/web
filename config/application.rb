24032be46a67910bccfca6584632c1a244371305
Boris Barroso <boriscyber@gmail.com>
2013-09-13 13:23:10 -0400

    Add font awesome to path

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module BonsaiErpSite
     config.i18n.default_locale = :es
 
     config.assets.paths << Rails.root.join('vendor', 'assets')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 
     config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
   end
